592ca524c0088e45ab9bd8a63ececb58df7f49eb
Thompson <tigredonorte3@gmail.com>
2023-11-07 17:17:55 -0300

    --amend

--- src/commands/back/back.ts
+++ src/commands/back/back.ts
@@ -8,6 +8,7 @@ export class BackCommand implements ICommand {
     private childrenHelper: IChildrenHelper = new ChildrenHelper(git),
     private parentHelper: IParentHelper = new ParentHelper(git),
     private branchHelper: IBranchHelper = new BranchHelper(git),
+    private logger = console,
   ) {}
 
   public help(): string {
@@ -25,13 +26,13 @@ export class BackCommand implements ICommand {
     if (backIndex >= 0) {
       await this.checkout(siblings[backIndex], currentBranch);
     } else {
-      console.log('No previous sibling branch found.');
+      this.logger.log('No previous sibling branch found.');
     }
   }
 
   private async checkout(targetBranch: string, currentBranch: string): Promise<void> {
     await this.git.checkout(targetBranch);
-    console.log(`Switched from ${currentBranch} to ${targetBranch}`);
+    this.logger.log(`Switched from ${currentBranch} to ${targetBranch}`);
   }
 
   private async getSiblings(currentBranch: string): Promise<string[]> {
--- src/commands/next/next.ts
+++ src/commands/next/next.ts
@@ -8,6 +8,7 @@ export class NextCommand implements ICommand {
     private childrenHelper: IChildrenHelper = new ChildrenHelper(git),
     private parentHelper: IParentHelper = new ParentHelper(git),
     private branchHelper: IBranchHelper = new BranchHelper(git),
+    private logger = console,
   ) {}
 
   public help(): string {
@@ -25,13 +26,13 @@ export class NextCommand implements ICommand {
     if (nextIndex < siblings.length) {
       await this.checkout(siblings[nextIndex], currentBranch);
     } else {
-      console.log('No next sibling branch found.');
+      this.logger.log('No next sibling branch found.');
     }
   }
 
   private async checkout(targetBranch: string, currentBranch: string): Promise<void> {
     await this.git.checkout(targetBranch);
-    console.log(`Switched from ${currentBranch} to ${targetBranch}`);
+    this.logger.log(`Switched from ${currentBranch} to ${targetBranch}`);
   }
 
   private async getSiblings(currentBranch: string): Promise<string[]> {
