481b8698ccf53eb43ab3e0e7f627d3a0c56caa7b
Thompson <tigredonorte3@gmail.com>
2023-11-07 17:15:24 -0300

    adds next and back commands

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,6 +1,8 @@
 import { ICommand } from './ICommand';
+import { BackCommand } from './back/back';
 import { CommitCommand } from './commit/commit';
 import { DownCommand } from './down/down';
+import { NextCommand } from './next/next';
 import { SyncUpCommand } from './syncUp/syncUp';
 import { UpCommand } from './up/up';
 
@@ -9,4 +11,6 @@ export const CommandRegistry: Record<string, ICommand> = {
   up: new UpCommand(),
   syncUp: new SyncUpCommand(),
   commit: new CommitCommand(),
+  next: new NextCommand(),
+  back: new BackCommand(),
 };
---
+++ src/commands/next/next.ts
@@ -0,0 +1,42 @@
+import simpleGit, { SimpleGit } from 'simple-git';
+import { BranchHelper, ChildrenHelper, IBranchHelper, IChildrenHelper, IParentHelper, ParentHelper } from '../../helpers';
+import { ICommand } from '../ICommand';
+
+export class NextCommand implements ICommand {
+  public constructor(
+    private git: SimpleGit = simpleGit(),
+    private childrenHelper: IChildrenHelper = new ChildrenHelper(git),
+    private parentHelper: IParentHelper = new ParentHelper(git),
+    private branchHelper: IBranchHelper = new BranchHelper(git),
+  ) {}
+
+  public help(): string {
+    return 'Usage: zgit next\n' +
+           'Switches to the next sibling branch if available.';
+  }
+
+  public async execute(): Promise<void> {
+    const currentBranch = await this.branchHelper.getCurrentBranch();
+    const siblings = await this.getSiblings(currentBranch);
+    
+    const currentIndex = siblings.findIndex((branch: string) => branch === currentBranch);
+    const nextIndex = currentIndex + 1;
+
+    if (nextIndex < siblings.length) {
+      await this.checkout(siblings[nextIndex], currentBranch);
+    } else {
+      console.log('No next sibling branch found.');
+    }
+  }
+
+  private async checkout(targetBranch: string, currentBranch: string): Promise<void> {
+    await this.git.checkout(targetBranch);
+    console.log(`Switched from ${currentBranch} to ${targetBranch}`);
+  }
+
+  private async getSiblings(currentBranch: string): Promise<string[]> {
+    const parent = await this.parentHelper.getParent(currentBranch);
+    const siblings = await this.childrenHelper.getChildren(parent.firstParent);
+    return siblings;
+  }
+}
