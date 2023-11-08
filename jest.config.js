1f3f738f315c75266a26dca604c4f508c607e3cb
Thompson <tigredonorte3@gmail.com>
2023-11-08 10:40:30 -0300

    adds inversify to repository

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,5 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/src/**/*.test.ts', '**/src/**/*.spec.ts'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
---
+++ src/inversify/types.ts
@@ -0,0 +1,10 @@
+const TYPES = {
+  SimpleGit: Symbol.for('SimpleGit'),
+  BranchHelper: Symbol.for('BranchHelper'),
+  ChildrenHelper: Symbol.for('ChildrenHelper'),
+  PrefixHelper: Symbol.for('PrefixHelper'),
+  ParentHelper: Symbol.for('ParentHelper'),
+  LoggerHelper: Symbol.for('LoggerHelper'),
+};
+
+export default TYPES;
