43baeb21dd35842faa6e337757bb42f00ca239b1
Thompson <tigredonorte3@gmail.com>
2024-09-06 18:23:30 -0300

    Adding an error handler for uncaught exceptions in the main process [(pull request #PR10)](https://github.com/tigredonorte/zgit-cli/pull/10)

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,26 @@ import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 import { CommandRegistry } from './commands';
 
+process.on('uncaughtException', (error) => {
+  console.error(error.message);
+  process.exit(1); // Exit with a failure code
+});
+
 async function main() {
-  const yargsInstance = yargs(hideBin(process.argv));
+  const yargsInstance = yargs(hideBin(process.argv))
+    .strict()
+    .help()
+    .alias('h', 'help')
+    .fail((msg, err, yargs) => {
+      if (err) {
+        return;
+      }
+
+      console.error('Invalid command:', msg);
+      console.error(yargs.help()); 
+      process.exit(1);
+    });
+
 
   Object.keys(CommandRegistry).sort().map((commandName) => {
     const command = CommandRegistry[commandName];
